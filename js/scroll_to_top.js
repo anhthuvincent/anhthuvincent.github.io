2b70e70c33dc581c65b08042daffa818191ed32a
anhthuvincent <anhthuvincent@users.noreply.github.com>
2020-12-18 11:50:18 +0100

    Scroll to top button

---
+++ js/scroll_to_top.js
@@ -0,0 +1,21 @@
+﻿// Get button and set content
+scrollBtn = document.getElementById("ScrollToTopBtn");
+scrollBtn.title = "Back to top";
+scrollBtn.innerHTML = "<span class='material-icons'>north</span>";
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {setScrollBtnVisibility()};
+
+function setScrollBtnVisibility() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.visibility = "hidden";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
